49853a7431f3be780179daa25fdf1e75259f79a8
Fabio Dela Bruna <fabio.bruna@agrotis.com>
2021-12-05 10:13:51 -0300

    Primeiro commit

---
+++ src/main/java/fabiodelabruna/starwars/resistence/socialnetwork/StarWarsResistenceSocialNetworkApplication.java
@@ -0,0 +1,13 @@
+package fabiodelabruna.starwars.resistence.socialnetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarWarsResistenceSocialNetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarWarsResistenceSocialNetworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fabiodelabruna/starwars/resistence/socialnetwork/StarWarsResistenceSocialNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package fabiodelabruna.starwars.resistence.socialnetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarWarsResistenceSocialNetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
